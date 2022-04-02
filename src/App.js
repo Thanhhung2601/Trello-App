ef4f73835d0a7cdadfc14f31d3735052cae8086d
hung <anhkp222@gmail.com>
2022-04-02 16:35:36 +0700

    Completed Design.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,169 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <nav className="navbar app">AppBar</nav>
+            <nav className="navbar board">Board bar</nav>
+            <div className="board-columns">
+                <div className="column">
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img
+                                src="https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.6435-9/177565248_2906957672917278_1600894721905628046_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=yGJfykgRF1kAX_RdAgU&tn=CFxQT_hdXFTMvGtv&_nc_ht=scontent.fsgn2-4.fna&oh=00_AT86JZq40MSICwHlv9voq0gQ9Fhx0WU_0X__UWgDWi_q3Q&oe=626C19A1"
+                                alt=""
+                            />
+                        </li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                    </ul>
+                    <footer>Add another card </footer>
+                </div>
+                <div className="column">
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img
+                                src="https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.6435-9/177565248_2906957672917278_1600894721905628046_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=yGJfykgRF1kAX_RdAgU&tn=CFxQT_hdXFTMvGtv&_nc_ht=scontent.fsgn2-4.fna&oh=00_AT86JZq40MSICwHlv9voq0gQ9Fhx0WU_0X__UWgDWi_q3Q&oe=626C19A1"
+                                alt=""
+                            />
+                        </li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                    </ul>
+                    <footer>Add another card </footer>
+                </div>
+                <div className="column">
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img
+                                src="https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.6435-9/177565248_2906957672917278_1600894721905628046_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=yGJfykgRF1kAX_RdAgU&tn=CFxQT_hdXFTMvGtv&_nc_ht=scontent.fsgn2-4.fna&oh=00_AT86JZq40MSICwHlv9voq0gQ9Fhx0WU_0X__UWgDWi_q3Q&oe=626C19A1"
+                                alt=""
+                            />
+                        </li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                    </ul>
+                    <footer>Add another card </footer>
+                </div>
+                <div className="column">
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img
+                                src="https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.6435-9/177565248_2906957672917278_1600894721905628046_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=yGJfykgRF1kAX_RdAgU&tn=CFxQT_hdXFTMvGtv&_nc_ht=scontent.fsgn2-4.fna&oh=00_AT86JZq40MSICwHlv9voq0gQ9Fhx0WU_0X__UWgDWi_q3Q&oe=626C19A1"
+                                alt=""
+                            />
+                        </li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                    </ul>
+                    <footer>Add another card </footer>
+                </div>
+                <div className="column">
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img
+                                src="https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.6435-9/177565248_2906957672917278_1600894721905628046_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=yGJfykgRF1kAX_RdAgU&tn=CFxQT_hdXFTMvGtv&_nc_ht=scontent.fsgn2-4.fna&oh=00_AT86JZq40MSICwHlv9voq0gQ9Fhx0WU_0X__UWgDWi_q3Q&oe=626C19A1"
+                                alt=""
+                            />
+                        </li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                    </ul>
+                    <footer>Add another card </footer>
+                </div>
+                <div className="column">
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img
+                                src="https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.6435-9/177565248_2906957672917278_1600894721905628046_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=yGJfykgRF1kAX_RdAgU&tn=CFxQT_hdXFTMvGtv&_nc_ht=scontent.fsgn2-4.fna&oh=00_AT86JZq40MSICwHlv9voq0gQ9Fhx0WU_0X__UWgDWi_q3Q&oe=626C19A1"
+                                alt=""
+                            />
+                        </li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                    </ul>
+                    <footer>Add another card </footer>
+                </div>
+                <div className="column">
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img
+                                src="https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.6435-9/177565248_2906957672917278_1600894721905628046_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=yGJfykgRF1kAX_RdAgU&tn=CFxQT_hdXFTMvGtv&_nc_ht=scontent.fsgn2-4.fna&oh=00_AT86JZq40MSICwHlv9voq0gQ9Fhx0WU_0X__UWgDWi_q3Q&oe=626C19A1"
+                                alt=""
+                            />
+                        </li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                        <li>Keep Learning reactjs</li>
+                    </ul>
+                    <footer>Add another card </footer>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
